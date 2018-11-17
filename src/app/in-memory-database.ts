38acba3f118e6bf2a380f2f1a28e66bd7c740c16
José Aparecido Silvano de Albuquerque <silvanoalbuquerque@gmail.com>
2018-11-17 17:04:33 -0300

    Fixing categories ids in database and in edition

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -6,10 +6,10 @@ export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
         const categories: Category[] = [
             { id: 1, name: 'Moradia', description: 'Pagamento de Contas da Casa' },
-            { id: 1, name: 'Saúde', description: 'Plano de saúde e Remédios' },
-            { id: 1, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
-            { id: 1, name: 'Salário', description: 'Recebimento de salário' },
-            { id: 1, name: 'Freelas', description: 'Trabalhos como freelancer' }
+            { id: 2, name: 'Saúde', description: 'Plano de saúde e Remédios' },
+            { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
+            { id: 4, name: 'Salário', description: 'Recebimento de salário' },
+            { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer' }
         ];
 
         return { categories };
